95a0d32f0a1a6ac0cd7eb68ee3ba6a7b3b1c8e9a
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-03-13 10:14:21 +0600

    #28 Added code in Main Activity
     Added code for spinners in Main Activity. Spinner for "School" & "Departments"

--- Theory/Assignment/Assignment02/app/src/main/java/com/example/studentsignupapp/MainActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/studentsignupapp/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner schoolSpinner;
     private Spinner departmentSpinner ;
-    private ArrayAdapter<CharSequence> arrayAdapterDepartments;
-    private ArrayAdapter<CharSequence> arrayAdapterSchool;
+    private ArrayAdapter<String> arrayAdapterDepartments;
+    private ArrayAdapter<String> arrayAdapterSchool;
 
 
 
@@ -20,42 +20,53 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //------------------------------SPINNER CODE FOR SCHOOL-----------------------
         schoolSpinner = (Spinner) findViewById(R.id.school_spinner);
+        schoolSpinner.setOnItemSelectedListener(this);
         departmentSpinner = (Spinner) findViewById(R.id.department_spinner);
-        String[] school = getResources().getStringArray(R.array.school_names);
+        final String[] school = getResources().getStringArray(R.array.school_names);
         arrayAdapterSchool =  new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,school);
-        arrayAdapterSchool.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        arrayAdapterSchool.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         schoolSpinner.setAdapter(arrayAdapterSchool);
+        //-------------------------------------END------------------------------------
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //---------------------------------SPINNER CODE FOR DEPARTMENTS------------------------------------
         if(position == 0){
-            String[] sbeDepartments = getResources().getStringArray(R.array.sbe_department_spinner);
+            final String[] sbeDepartments = getResources().getStringArray(R.array.sbe_department_spinner);
             arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,sbeDepartments);
-            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             departmentSpinner.setAdapter(arrayAdapterDepartments);
-        }
-        if(position == 1){
-            String[] sepDepartments = getResources().getStringArray(R.array.sep_department_spinner);
-            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,sepDepartments);
-            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-            departmentSpinner.setAdapter(arrayAdapterDepartments);
-        }
-        if(position == 2){
-            String[] shlDepartments = getResources().getStringArray(R.array.shl_department_spinner);
-            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,shlDepartments);
-            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-            departmentSpinner.setAdapter(arrayAdapterDepartments);
-        }
-        if(position == 3){
-            String[] shsDepartments = getResources().getStringArray(R.array.shs_department_spinner);
-            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,shsDepartments);
-            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-            departmentSpinner.setAdapter(arrayAdapterDepartments);
-        }
 
+        }else
+        if(position == 1){
+            final String[] sepDepartments = getResources().getStringArray(R.array.sep_department_spinner);
+            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,sepDepartments);
+            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            departmentSpinner.setAdapter(arrayAdapterDepartments);
+
+
+        }else
+        if(position == 2){
+            final String[] shlDepartments = getResources().getStringArray(R.array.shl_department_spinner);
+            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,shlDepartments);
+            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            departmentSpinner.setAdapter(arrayAdapterDepartments);
+
+
+        }else
+        if(position == 3){
+            final String[] shsDepartments = getResources().getStringArray(R.array.shs_department_spinner);
+            arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,shsDepartments);
+            arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            departmentSpinner.setAdapter(arrayAdapterDepartments);
+
+
+        }
+        //----------------------------------------------END--------------------------------------------------------
 
     }
 
