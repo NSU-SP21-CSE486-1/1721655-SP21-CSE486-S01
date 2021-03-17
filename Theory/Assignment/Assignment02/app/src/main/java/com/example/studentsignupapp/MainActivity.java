8543cf2def07e504148a26e1248ae4629428c8f7
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-03-18 01:05:27 +0600

    #30 Date of Birth, Phone no., NID

--- Theory/Assignment/Assignment02/app/src/main/java/com/example/studentsignupapp/MainActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/studentsignupapp/MainActivity.java
@@ -1,26 +1,39 @@
 package com.example.studentsignupapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.hbb20.CountryCodePicker;
+
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner schoolSpinner;
     private Spinner departmentSpinner ;
     private ArrayAdapter<String> arrayAdapterDepartments;
     private ArrayAdapter<String> arrayAdapterSchool;
+    private EditText dateOfBirth,phoneNumber;
+    private CountryCodePicker ccp;
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //------------------------------SPINNER CODE FOR SCHOOL-----------------------
+
+        //------------------------------SPINNER CODE FOR SCHOOL-------------------------------------
         schoolSpinner = (Spinner) findViewById(R.id.school_spinner);
         schoolSpinner.setOnItemSelectedListener(this);
         departmentSpinner = (Spinner) findViewById(R.id.department_spinner);
@@ -28,13 +41,39 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         arrayAdapterSchool =  new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,school);
         arrayAdapterSchool.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         schoolSpinner.setAdapter(arrayAdapterSchool);
-        //-------------------------------------END------------------------------------
+        //-------------------------------------END--------------------------------------------------
+
+        //--------------------------------CODE FOR DATE OF BIRTH------------------------------------
+        dateOfBirth = findViewById(R.id.dob_editText);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateOfBirth.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                DatePickerDialog datePicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month = month + 1;
+                        String date = dayOfMonth + "/" + month + "/" + year;
+                        dateOfBirth.setText(date);
+                    }
+                },year,month,day);
+                datePicker.show();
+            }
+        });
+        //---------------------------------------------END------------------------------------------
+
+        //---------------------------------CODE FOR PHONE NUMBER------------------------------------
+        ccp = (CountryCodePicker) findViewById(R.id.picker);
+        phoneNumber = (EditText) findViewById(R.id.phone_number_editText);
 
     }
-
+    //---------------------------------SPINNER CODE FOR DEPARTMENTS---------------------------------
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //---------------------------------SPINNER CODE FOR DEPARTMENTS------------------------------------
         if(position == 0){
             final String[] sbeDepartments = getResources().getStringArray(R.array.sbe_department_spinner);
             arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,sbeDepartments);
@@ -48,14 +87,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             departmentSpinner.setAdapter(arrayAdapterDepartments);
 
-
         }else
         if(position == 2){
             final String[] shlDepartments = getResources().getStringArray(R.array.shl_department_spinner);
             arrayAdapterDepartments = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,shlDepartments);
             arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             departmentSpinner.setAdapter(arrayAdapterDepartments);
-
 
         }else
         if(position == 3){
@@ -64,12 +101,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             arrayAdapterDepartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             departmentSpinner.setAdapter(arrayAdapterDepartments);
 
-
         }
-        //----------------------------------------------END--------------------------------------------------------
-
     }
-
+    //----------------------------------------------END---------------------------------------------
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
