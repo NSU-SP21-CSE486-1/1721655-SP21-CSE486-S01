bbff2e0388f927d8bd092fd31b19739bdb8ff439
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-03-19 21:06:58 +0600

    #31 Lab 04
    Assignment work for Lab-04

---
+++ Lab/Lab04/SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2018, Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.SimpleCalc;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.Display;
+import android.widget.Toast;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
+ */
+@RunWith(JUnit4.class)
+@SmallTest
+public class CalculatorTest {
+    private Calculator mCalculator;
+
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
+    }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+    /**
+     * Test for simple addition with negative number
+     */
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+
+    /**
+     * Test for simple addition with floating point number
+     */
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+        assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
+
+    /**
+     * Test for simple subtraction
+     */
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = mCalculator.sub(1d, 1d);
+        assertThat(resultSub, is(equalTo(0d)));
+    }
+
+    /**
+     * Test for simple subtraction with negative result
+     */
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = mCalculator.sub(1d, 17d);
+        assertThat(resultSub, is(equalTo(-16d)));
+    }
+
+    /**
+     * Test for simple multiplication
+     */
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = mCalculator.mul(32d, 2d);
+        assertThat(resultMul, is(equalTo(64d)));
+    }
+
+    /**
+     * Test for simple division
+     */
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = mCalculator.div(32d, 2d);
+        assertThat(resultDiv, is(equalTo(16d)));
+    }
+
+    /**
+     * Test for simple division by zero
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void divByZeroThrows() {
+        double resultDiv = mCalculator.div(32d, 0);
+        throw new IllegalArgumentException();
+    }
+
+
+
+}
