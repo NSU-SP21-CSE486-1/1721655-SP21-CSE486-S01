f77df26b2cff0162662a7c159aa326521e20accb
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-04-12 16:23:46 +0600

    #50 Splash Screen

--- Project/Version01/CPCApp/app/src/main/java/com/example/cpcapp/MainActivity.java
+++ Project/Version01/CPCApp/app/src/main/java/com/example/cpcapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.cpcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+import com.example.cpcapp.ui.Home;
 
 public class MainActivity extends AppCompatActivity {
+    private static int TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        new Handler(Looper.myLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME);
     }
 }
---
+++ Project/Version01/CPCApp/app/src/main/java/com/example/cpcapp/ui/Home.java
@@ -0,0 +1,14 @@
+package com.example.cpcapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.cpcapp.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
