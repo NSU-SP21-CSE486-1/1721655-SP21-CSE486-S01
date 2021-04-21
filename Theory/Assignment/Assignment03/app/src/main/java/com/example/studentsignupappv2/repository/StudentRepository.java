8f294d9d08860ce5f0fd0415ef262ec3c08a60bb
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-04-21 17:32:43 +0600

    #54 Student Repository for Firebase Database

--- Theory/Assignment/Assignment03/app/src/main/java/com/example/studentsignupappv2/repository/StudentRepository.java
+++ Theory/Assignment/Assignment03/app/src/main/java/com/example/studentsignupappv2/repository/StudentRepository.java
@@ -1,6 +1,33 @@
 package com.example.studentsignupappv2.repository;
 
+
+
+import com.example.studentsignupappv2.datascource.StudentEntity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class StudentRepository {
+
+ private static final int THREADS_NUMBER= 4;
+ private static final ExecutorService threadExecutor = Executors.newFixedThreadPool(THREADS_NUMBER);
+ private FirebaseDatabase firebaseRef;
+ private DatabaseReference dbRef;
+
+ public StudentRepository(){
+  firebaseRef = FirebaseDatabase.getInstance();
+  dbRef =  firebaseRef.getReference("user");
+ }
+
+ public static ExecutorService getThreadExecutor(){return threadExecutor;}
+
+ public void insert(StudentEntity student){
+   dbRef.setValue(student);
+ }
+
 
 
 }
