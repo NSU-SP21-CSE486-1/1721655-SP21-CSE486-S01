c4b38dffd8a65b2ef16215218839c8ab13a72fce
sayem499 <79084263+sayem499@users.noreply.github.com>
2021-05-17 10:09:55 +0600

    #73 Login Function Implemented

--- Project/Version01/CPCApp/app/src/main/java/com/example/cpcapp/ui/Login.java
+++ Project/Version01/CPCApp/app/src/main/java/com/example/cpcapp/ui/Login.java
@@ -1,20 +1,67 @@
 package com.example.cpcapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cpcapp.R;
+import com.example.cpcapp.viewmodel.AppViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private EditText nsuEmail,mPassword;
+    private TextView mRegister;
+    private AppViewModel appViewModel;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        appViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(this.getApplication())).get(AppViewModel.class);
+
+        nsuEmail = findViewById(R.id.log_nsu_email_editText);
+        mPassword = findViewById(R.id.log_password_editText);
+        mRegister = findViewById(R.id.log_register_textView);
+
+        firebaseAuth = appViewModel.getAuthRef();
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Registration.class));
+                finish();
+            }
+        });
+
     }
 
     public void Login(View view) {
+
+        firebaseAuth.signInWithEmailAndPassword(nsuEmail.getText().toString(),mPassword.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Login Successful!!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), Home.class));
+                    finish();
+                }
+            }
+        });
+
     }
 }
